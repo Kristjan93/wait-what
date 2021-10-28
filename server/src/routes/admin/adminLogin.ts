2ce86c013586c62665dc852b9c6d48d2f135a3f1
Kristjan93 <krillinn@gmail.com>
2021-10-28 18:21:56 +0000

    deploy

--- server/src/routes/admin/adminLogin.ts
+++ server/src/routes/admin/adminLogin.ts
@@ -22,7 +22,8 @@ export const adminLogin = (req: Request, res: Response) => {
 
   const options: CookieSerializeOptions = {
     maxAge: expireTime,
-    httpOnly: process.env.NODE_ENV !== 'development',
+    // httpOnly: process.env.NODE_ENV !== 'development',
+    httpOnly: false,
     domain: process.env.NODE_ENV === 'production' ? 'https://wait-what.vercel.app' : '/'
     // secure: process.env.NODE_ENV !== 'development',
   }
